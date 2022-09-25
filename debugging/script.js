7dfd9df0706afdb2b4b3dd831bb1998ffbe430a1
dwadasi siva teja <sivateja1034@gmail.com>
2022-09-25 20:02:29 +0530

    coding challenge (#2)
    completing the pull request

--- debugging/script.js
+++ debugging/script.js
@@ -1,16 +1,24 @@
-
-
 // function for the converting into the temperature to the kelvin
-const convertToKelvin = function () {
-    const measurement = {
-        type: 'temperature',
-        unit: 'celsius',
-        value: Number(prompt('degree celsius'))
-    }
 
-    const kelvin = measurement.value + 273;
-    return kelvin;
-}
+// const convertToKelvin = function () {
+//     const measurement = {
+//         type: 'temperature',
+//         unit: 'celsius',
+//         value: Number(prompt('degree celsius'))
+//     }
 
+//     const kelvin = measurement.value + 273;
+//     return kelvin;
+// }
+// console.log(convertToKelvin());
 
-console.log(convertToKelvin());
+/** coding challenge */
+
+tempArray = [17, 21, 23]
+let tempString = ''
+tempArray.forEach((element, index) => {
+    tempString = tempString + `... ${element} degree *c in ${index + 1} days`;
+});
+
+console.log(tempString)
+
