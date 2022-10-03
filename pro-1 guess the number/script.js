332290e9838f2e3d7e0e75d2db3dca1e6f6a6315
dwadasi siva teja <sivateja1034@gmail.com>
2022-10-03 15:19:49 +0530

    wip

--- pro-1 guess the number/script.js
+++ pro-1 guess the number/script.js
@@ -1,1 +1,14 @@
 'use strict';
+
+
+
+// for changing the  number in the box 
+document.querySelector('.number').textContent = 11;
+
+
+// side note message 
+document.querySelector('.message ').textContent = '🎉 correct Number!';
+
+// for score selector 
+document.querySelector(".score").textContent = '19';
+
