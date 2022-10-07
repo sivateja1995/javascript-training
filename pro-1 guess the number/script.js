0d4f84e23f8c721d3891a0194abb92fc6f4d9ecb
dwadasi siva teja <sivateja1034@gmail.com>
2022-10-07 23:11:11 +0530

    Project1/guess the number (#3)
    * [new] project guess the number
    
    * wip
    
    * added the changes in the gitIgnore
    
    * [feature] work in progress
    
    * [feature] work in progress
    
    * logic implementation completed
    
    * wip

---
+++ pro-1 guess the number/script.js
@@ -0,0 +1,49 @@
+'use strict';
+
+// function to generate the random number
+function generateRandomInt(max) {
+    return Math.floor(Math.random() * max);
+}
+const secretNumber = generateRandomInt(20);
+document.querySelector(".highscore").textContent = localStorage.getItem('highScore');
+/** 
+// for changing the  number in the box 
+document.querySelector('.number').textContent = 11;
+
+// side note message 
+document.querySelector('.message ').textContent = '🎉 correct Number!';
+// for score selector 
+document.querySelector(".score").textContent = '19';
+
+// for the input value 
+document.querySelector(".guess").value = 16;
+*/
+
+
+
+/** creating the click event function  */
+const guess = function checkTheGuess() {
+    console.log('button is clicked');
+    const guessNumber = Number(document.querySelector('.guess').value);
+    if (!!guessNumber) {
+        if (guessNumber > secretNumber) {
+            document.querySelector('.message ').textContent = 'too High !!';
+            document.querySelector(".score").textContent = Number(document.querySelector(".score").textContent) - 1;
+        } else if (guessNumber < secretNumber) {
+            document.querySelector('.message ').textContent = 'too less !!';
+            document.querySelector(".score").textContent = Number(document.querySelector(".score").textContent) - 1;
+        } else if (guessNumber === secretNumber) {
+            document.querySelector('.message ').textContent = '🎉 correct Number!';
+            if (Number(document.querySelector(".score").textContent) > Number(localStorage.getItem('highScore'))) {
+                const highScore = Number(document.querySelector(".score").textContent);
+                localStorage.setItem('highScore', highScore);
+                document.querySelector(".highscore").textContent = localStorage.getItem('highScore');
+            }
+        }
+    } else {
+        document.querySelector('.message').textContent = 'No Number Entered!! ⛔'
+    }
+}
+
+// creating the click event when the button is pressed
+document.querySelector('.check').addEventListener('click', guess);
