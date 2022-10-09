02de7d3cafa7d4cbc81a60175712fcd58197795f
dwadasi siva teja <sivateja1034@gmail.com>
2022-10-09 23:02:54 +0530

    Feature/project2 (#6)
    * [feature] initial commit
    
    * wip
    
    * completed the second project

--- pro-2 window modal/script.js
+++ pro-2 window modal/script.js
@@ -1,1 +1,23 @@
 'use strict';
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnsOpenModal = document.querySelectorAll('.show-modal');
+
+// closing of the classes
+const hideModal = () => {
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
+};
+
+// showing of the modal
+const showModal = () => {
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+};
+
+for (let i = 0; i < btnsOpenModal.length; i++) {
+    btnsOpenModal[i].addEventListener('click', showModal);
+}
+
+btnCloseModal.addEventListener('click', hideModal);
