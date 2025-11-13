285e6e32dbb306ccd94daca33ee25141b98e5f28
sivateja1995 <sivateja1034@gmail.com>
2025-11-13 22:02:37 +0530

    added the changes

--- 03.variables/01.js
+++ 03.variables/01.js
@@ -2,15 +2,14 @@ let admin;
 let name;
 name = "John";
 admin = name;
-console.log(admin);
 
 // calcualte the age
 const birthday = "18.04.1982";
 const fromattedBirthday = birthday.split(".").reverse().join("-");
-console.log('formatted birthday:',fromattedBirthday);   
-const birthDate = new Date(fromattedBirthday);
-console.log('year',new Date(birthDate).getFullYear());
 
-const age = new Date().getFullYear() - new Date(fromattedBirthday).getFullYear();
-console.log(age);
+const birthDate = new Date(fromattedBirthday);
+
+const age =
+  new Date().getFullYear() - new Date(fromattedBirthday).getFullYear();
+
 // -- end of file --s
