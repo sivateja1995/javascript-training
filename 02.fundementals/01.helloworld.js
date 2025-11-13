3075fff14cae254dc26e3c52580c51a4434570c4
sivateja1995 <sivateja1034@gmail.com>
2025-11-13 22:03:04 +0530

    added new changes

---
+++ 02.fundementals/01.helloworld.js
@@ -0,0 +1,19 @@
+let user = "hima bindu";
+var age = "26";
+var message = "don't marry be happy";
+
+// second way of initializing variables
+let user1 = "siva teja";
+var age1 = "26";
+var message1 = "don't worry be happy";
+
+// testing the variable deep copy
+age = age1;
+age1 = "30";
+message = message1;
+message1 = "marry and be happy";
+
+var a = (b = "test");
+b = "best";
+
+// -- end of file --
